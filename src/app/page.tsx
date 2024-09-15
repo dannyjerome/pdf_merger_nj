3166c7e19eb398b1afa2cd3a2a1a9d690581b463
Danny Jerome <dannystreak925@gmail.com>
2024-09-15 20:54:27 +0530

    chore: other updates to help with the frontend

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,19 @@
+
 import Features from "@/app/components/Features";
 import Hero from "@/app/components/Hero";
-import SignUp from "./components/SignUp";
+import Footer from "@/app/components/Footer";
+import SignUp from "@/app/components/SignUp";
 
 export default function Home() {
   return (
-    <div className=" flex flex-col items-center justify-center">
-      <Hero />
-      <Features />
-      <SignUp />
-    </div>
+
+    <main>
+      <div className=" flex flex-col items-center justify-center">
+        <Hero />
+        <Features />
+        <SignUp />
+        <Footer />
+      </div>
+    </main>
   );
 }
